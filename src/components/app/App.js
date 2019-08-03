443437408c5abc575f2d387baa6b9a6019b255b1
Amber <amber__12@hotmail.com>
2019-08-03 12:37:26 +0200

    Remove standard react stuff and add sass

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import '../../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CSV table</h1>
+    </div>
+  );
+}
+
+export default App;
