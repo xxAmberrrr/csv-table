5a1deaac82c98ce5c8cc4e36ff4f29dae4390729
Amber <amber__12@hotmail.com>
2019-08-04 18:14:32 +0200

    Upload csv and put data inside table

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,89 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
-import '../../styles/App.scss';
+import '../../styles/main.scss';
+import CSVReader from 'react-csv-reader';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>CSV table</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataIssue: [],
+      bodyData: [],
+      sort: false,
+    }
+  }
+
+  sortAsc = () => {
+    const { bodyData } = this.state;
+
+    const sortedData = bodyData.sort(function(a, b) {
+      return a[1] < b[1] ? 1 : -1
+    })
+
+    this.setState({
+      bodyData: sortedData,
+      sort: true,  
+    })
+  }
+
+  sortDesc = () => {
+    const { bodyData } = this.state;
+
+    const sortedData = bodyData.sort(function(a, b) {
+      return a[1] < b[1] ? -1 : 1
+    })
+
+    this.setState({
+      bodyData: sortedData,
+      sort: false,  
+    })
+  }
+
+
+  handleCSV = data => {
+    this.setState({
+      dataIssue: data,
+      bodyData: data.slice(1).sort(function(a, b) {
+        return a[1] < b[1] ? -1 : 1;
+      })
+    })
+  }
+
+  renderTableHead = () => {
+    if(this.state.dataIssue[0] !== undefined) {
+      var row = this.state.dataIssue[0].map(cell => <th onClick={() => this.state.sort ? this.sortDesc() : this.sortAsc()}>{cell}</th>);
+      return <tr>{row}</tr>
+    }
+  }
+
+  renderTableBody = () => {
+    if(this.state.bodyData !== undefined) {
+      return this.state.bodyData.map(rows => {
+        var row = rows.map(cell => <td>{cell}</td>);
+        return <tr>{row}</tr>
+      })
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>CSV Table</h1>
+
+        <CSVReader
+          cssClass="csv-reader-input"
+          onFileLoaded={this.handleCSV}
+        />
+
+        <table id="issues">
+          <thead>
+            {this.renderTableHead()}
+          </thead>
+          <tbody>
+            {this.renderTableBody()}
+          </tbody>
+        </table>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
